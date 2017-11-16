c42e89435a3baeebac71d5cf7c8951643bb63c16
ChildLee <471227142@qq.com>
2017-11-16 22:14:08 +0800

    commit

--- src/main/java/cn/controller/AdminController.java
+++ src/main/java/cn/controller/AdminController.java
@@ -1,9 +1,20 @@
 package cn.controller;
 
+import cn.entity.Admin;
+import cn.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/admin")
 public class AdminController {
+
+    @Autowired
+    private AdminService adminService;
+
+    @RequestMapping("/admin")
+    public Admin getAdminAll() {
+        return adminService.getAdminAll();
+    }
 }
