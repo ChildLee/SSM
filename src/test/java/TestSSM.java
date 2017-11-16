d72a701e47046056bf5277a2384e0fce713c0c67
ChildLee <471227142@qq.com>
2017-11-16 21:22:26 +0800

    commit

--- src/test/java/TestSSM.java
+++ src/test/java/TestSSM.java
@@ -1,14 +1,19 @@
+import cn.entity.Admin;
 import cn.service.AdminService;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:applicationContext.xml", "classpath:spring-mvc.xml"})
 public class TestSSM {
 
-    @Autowired
     private AdminService adminService;
 
     @Test
     public void mbts() throws Exception {
-
+        Admin ad = adminService.getAdminAll();
+        System.out.println(ad);
     }
 }
