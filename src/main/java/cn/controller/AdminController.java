13284b7b3743fa76409000f4105f13f8520404de
ChildLee <471227142@qq.com>
2017-11-27 23:27:57 +0800

    commit

--- src/main/java/cn/controller/AdminController.java
+++ src/main/java/cn/controller/AdminController.java
@@ -3,13 +3,10 @@ package cn.controller;
 
 import cn.entity.Admin;
 import cn.service.AdminService;
-import cn.util.FtpUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
 
 
 @Controller
@@ -24,9 +21,5 @@ public class AdminController {
         return adminService.getAdminAll();
     }
 
-    @RequestMapping("/fileUpload")
-    public Boolean getfileUpload(@RequestParam("files") MultipartFile[] files) {
-        if (null == files) return false;
-        return FtpUtil.getfileUpload(files);
-    }
+
 }
---
+++ src/main/java/cn/controller/FileAction.java
@@ -0,0 +1,16 @@
+package cn.controller;
+
+import cn.util.FtpUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class FileAction {
+    @RequestMapping("/fileUpload")
+    public Boolean getfileUpload(@RequestParam("files") MultipartFile[] files) {
+        if (null == files) return false;
+        return FtpUtil.getfileUpload(files);
+    }
+}
